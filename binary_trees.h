4956e31d93501b1a7db4ab242c850745a5649c35
ShadyEgypt <shadyrafat60@gmail.com>
2023-08-05 04:06:37 +0300

    remaining tasks

--- binary_trees.h
+++ binary_trees.h
@@ -8,7 +8,7 @@
 #include <stdbool.h>
 #include <limits.h>
 
-
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 /* basic binary tree */
 
@@ -75,5 +75,17 @@ avl_t *avl_insert(avl_t **tree, int value);
 avl_t *array_to_avl(int *array, size_t size);
 avl_t *avl_remove(avl_t *root, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
+int binary_tree_is_heap(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *array_to_heap(int *array, size_t size);
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
+void binary_tree_print(const binary_tree_t *);
+avl_t *rebalance(avl_t *node, avl_t **tree);
+avl_t *balance_left(avl_t *node);
+avl_t *balance_right(avl_t *node);
+char *convert(unsigned long int num, int base, int lowercase);
+bst_t *swap(bst_t *a, bst_t *b);
+size_t _binary_tree_height(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
